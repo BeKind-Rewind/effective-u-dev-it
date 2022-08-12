49b975118434cf169dcf644dbb9896a4b44505e7
Amanda Perry <challenge641@gmail.com>
2022-08-12 18:47:31 -0500

    set up all of our db calls, currently commented out to set up routes

--- server.js
+++ server.js
@@ -22,10 +22,43 @@ const db = mysql.createConnection(
 );
 
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
+// Query the whole list of candidates
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
 
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(row);
+// });
+
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+
+// To confirm the server is connected and working in the browser
 app.get('/', (req, res) => {
     res.json({
       message: 'Hello World'
