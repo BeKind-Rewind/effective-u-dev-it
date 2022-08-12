ca41b46a4deabfdc7d7562c3080e688d0a1146c7
Amanda Perry <challenge641@gmail.com>
2022-08-12 18:27:44 -0500

    solved connection issue with local host and db displaying

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+// Connect to the db
+const db = mysql.createConnection(
+    {
+        host: '127.0.0.1',
+        // Your MySql username,
+        user: 'root',
+        // Your MySql password
+        password: 'AllThePuppies123!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World'
+    });
+});
 
 
 // Route handler for req not supported, default response for any other request (Not Found)
